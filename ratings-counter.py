11d1d1a873812777c720eb7a8a4e0e2836c5fe05
Varun Pillai <pillai.varun@outlook.com>
2017-03-20 20:51:58 -0400

    first commit

---
+++ ratings-counter.py
@@ -0,0 +1,13 @@
+from pyspark import SparkConf, SparkContext
+import collections
+
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile("ml-100k/u.data")
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items(),key=lambda t: t[0],reverse=True))
+for key, value in sortedResults.items():
+    print("%s %i" % (key, value))
